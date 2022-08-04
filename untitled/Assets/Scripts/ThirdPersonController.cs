9c1530d5ba6a4d5b74be0b7295ce1e014f998c1b
akyurektuna <43556281+akyurektuna@users.noreply.github.com>
2022-08-04 21:21:58 +0300

    fixing character controller and adding a terrain
    using polybrush for terrain
    added gravity to character controller

--- untitled/Assets/Scripts/ThirdPersonController.cs
+++ untitled/Assets/Scripts/ThirdPersonController.cs
@@ -10,6 +10,13 @@ public class ThirdPersonController : MonoBehaviour
     public GameObject itemcol;
 
     public float speed = 6f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    Vector3 velocity;
+    bool isGrounded;
     public float turnSmoothTime = 0.1f;
 
     public Interactable item = null;
@@ -18,15 +25,35 @@ public class ThirdPersonController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+  isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
-        if(direction.magnitude >= 0.1f){
-            float targetAngle = Mathf.Atan2(direction.x, direction.z)*Mathf.Rad2Deg + cam.eulerAngles.y;
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f, angle,0f);
-            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f)*Vector3.forward;
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
 
